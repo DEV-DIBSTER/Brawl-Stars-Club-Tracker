ad5c00194af35f6adc51a3bd3dfbe034cacf3c82
DIBSTERYT <76603072+DIBSTERYT@users.noreply.github.com>
2022-10-26 22:52:07 -0400

    Start receiving stuff.

---
+++ Endpoints/tracking/data/get.js
@@ -0,0 +1,27 @@
+const Express = require('express');
+const Configuration = require('../../config.json');
+
+const ClubData = require('../../Database/ClubData.js');
+const ClubGraphs = require('../../Database/ClubGraphs.js');
+const ClubLogs = require('../../Database/ClubLogs.js');
+const ClubTracking = require('../../Database/ClubTracking.js');
+const { isTag } = require('../../Functions/isTag.js');
+const { getClub } = require('../../Functions/getClub.js');
+
+module.exports = ({
+    /**
+     * @param {Express.Request} Request
+     * @param {Express.Response} Response 
+     */
+    run: async (Request, Response) => {
+        if(!Configuration.APITokens.some(Token => Token == Request.headers.password)) return Response.status(403).send('Invalid authentication.');
+
+        const Input = Request.params.clubTag;
+        if(isTag(Input.replace('#', '').replace('%23', '')) == false) return Response.status(404).send('Invalid club tag characters!');
+
+        const ClubResponse = await getClub(Input.replace('#', '').replace('%23', ''));
+        if(ClubResponse == "No Data Found") return Response.status(404).send('Invalid club tag characters!');
+
+        //Still need to code this.
+    }
+});
